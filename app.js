72cdb50505e7c49671e2d845d5f62af5bad90857
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-05 22:15:10 +0900

    :memo: #2.1 | 2D Context

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const canvas = document.getElementById("jsCanvas");
+canvas.width = 600;
+canvas.height = 600;
+
+const ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function startPainting() {
+  painting = true;
+}
 
 function stopPainting() {
   painting = false;
@@ -9,19 +19,18 @@ function stopPainting() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-}
-
-function onMouseDown(event) {
-  paint = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
